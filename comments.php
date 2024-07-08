ba17ea1f43db9658bf94e4f8d313d37f1780349b
Eoyz369 <113827000+Eoyz369@users.noreply.github.com>
2024-07-08 21:16:45 +0800

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,3 +1,5 @@
+
+<script src="<?php $this->options->themeUrl('assets/js/comments.js'); ?>"></script>
 <?php
 /*
  * @Author: Eoyz369
@@ -149,6 +151,15 @@ $this->comments()->to($comments);
         </form>
     </div>
 </div>
+  <script src="<?php $this->options->themeUrl('assets/js/comments.js'); ?>">document.addEventListener('DOMContentLoaded', function() {
+    var commentLevels = document.querySelectorAll('.comment-level');
+    commentLevels.forEach(function(level) {
+        var text = level.textContent.trim(); // 获取元素内容并去除首尾空格
+        var newContent = text.replace('comment-', ''); // 去掉 comment- 前缀
+        level.textContent = newContent; // 更新元素内容
+    });
+});
+</script>
 
 <?php else : ?>
 
